8f63cbd2ee6d1faf959100aef63611934fd59836
Roland Thiolliere <rolthiolliere@gmail.com>
2014-09-08 17:21:22 +0200

    added stronger pip requirements, see if we can weaken them later

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ setup(
     long_description=open('README.rst').read(),
     install_requires=[
         "python >= 2.7",
-        "h5py",
-        "numpy",
-        "scipy.sparse"
+        "h5py >= 2.3.0",
+        "numpy >= 1.8.0",
+        "scipy.sparse >= 0.13.0"
     ],
 )
