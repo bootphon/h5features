b05df0dc637ba9f325041d97fd3a2b57d395964c
Roland Thiolliere <rolthiolliere@gmail.com>
2014-09-09 14:14:07 +0200

    added rdt requirements

--- docs/conf.py
+++ docs/conf.py
@@ -12,13 +12,23 @@
 # serve to show the default.
 
 import sys, os
-import numpydoc
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
+# mocking for ReadTheDoc
+from mock import Mock as MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
+
+MOCK_MODULES = ['argparse', 'scipy', 'numpy', 'h5py', 'scipy.sparse']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration -----------------------------------------------------
 
