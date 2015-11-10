1370278c6c4b25ed34c330257154eb973e29ce6e
Mathieu Bernard <mathieu_bernard@laposte.net>
2015-11-10 16:07:37 +0100

    converter documented

--- h5features/converter.py
+++ h5features/converter.py
@@ -27,7 +27,31 @@ from .reader import Reader
 from .writer import Writer
 
 class Converter(object):
-    """This class allows convertion from various formats to h5features."""
+    """This class allows convertion from various formats to h5features.
+
+    * A *Converter* instance owns an h5features file and write converted
+      input files to it, in a specified group.
+
+    * An input file is converted to h5fatures using the *convert*
+      method, which choose a concrete conversion method based on the
+      input file extension.
+
+    * Supported extensions are:
+
+        * **.npz** for numpy NPZ files
+
+        * **.mat** for Octave/Matlab files
+
+        * **.h5** for h5features files. In this later case, the files
+            are simply converted to latest version of the h5features
+            data format
+
+
+    :param str filename: The h5features to write in.
+    :param str groupname: The group to write in *filename*
+    :param float chunk: Size a chunk in *filename*, in MBytes.
+
+    """
     def __init__(self, filename, groupname, chunk=0.1):
         self._writer = Writer(filename, chunk)
         self.groupname = groupname
@@ -41,9 +65,16 @@ class Converter(object):
         return data[labels]
 
     def close(self):
+        """Close the converter and release the owned h5features file."""
         self._writer.close()
 
     def convert(self, infile):
+        """Convert an input file to h5features based on its extension.
+
+        :raise IOError: if *infile* is not a valid file.
+        :raise IOError: if *infile* extension is not supported.
+
+        """
         if not os.path.isfile(infile):
             raise IOError('{} is not a valid file'.format(infile))
 
@@ -58,6 +89,7 @@ class Converter(object):
             raise IOError('Unknown file format for {}'.format(infile))
 
     def npz_convert(self, infile):
+        """Convert a numpy NPZ file to h5features."""
         data = np.load(infile)
         item = os.path.splitext(infile)[0]
         labels = self._labels(data)
@@ -65,6 +97,7 @@ class Converter(object):
         self._write(item, labels, features)
 
     def mat_convert(self, infile):
+        """Convert a Octave/Matlab file to h5features."""
         data = sio.loadmat(infile)
         item = os.path.splitext(infile)[0]
         labels = self._labels(data)
@@ -73,9 +106,7 @@ class Converter(object):
         self._write(item, labels, features)
 
     def h5features_convert(self, infile):
-        """Convert a h5features file to the latest version."""
-        # TODO no need to read all data, just rename it
-        # TODO test it !
+        """Convert a h5features file to the latest h5features version."""
         with h5py.File(infile, 'r') as f:
             groups = list(f.keys())
 
