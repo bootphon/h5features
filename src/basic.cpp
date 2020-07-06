9bf5b1acdd40092c172b56eb47b41d875fa0c387
Mathieu Bernard <mathieu_bernard@laposte.net>
2020-07-07 00:34:08 +0200

    minor

--- src/basic.cpp
+++ src/basic.cpp
@@ -41,17 +41,105 @@ h5features::item generate_item(const std::string& name, std::size_t size, std::s
 }
 
 
+const std::string program = R"code(#!/usr/bin/env python
+import numpy as np
+import h5features as h5f
+
+items = ['item1', 'item2', 'item3']
+times = [
+    np.asarray([[0, 1], [1, 2], [2, 3]]),
+    np.asarray([[0, 1], [0.5, 1.5], [1, 2]]),
+    np.asarray([[0, 1], [1, 2]])]
+features = [
+    np.asarray([[0, 1.0, 0], [1, 2, 0.0], [2.0, 3, 0]]),
+    np.asarray([[10.1, 10, 10], [5.2, 5, 5], [1.3, 2, 3]]),
+    np.asarray([[0, 1.0, 0], [1, 2, 0]])]
+
+items = h5f.Data(items, times, features)
+h5f.Writer('test.h5', version='1.1').write(items, 'features')
+)code";
+
+
+template<class T>
+std::ostream& operator<<(std::ostream& os, std::vector<T> v)
+{
+   os << "[";
+   for(std::size_t i = 0; i < v.size()-1; ++i)
+      os << v[i] << " ";
+   os << *(v.rbegin()) << "]";
+   return os;
+}
+
+
+void read_v11()
+{
+   int code = std::system(
+      (std::string("python -c \"") + program + std::string("\"")).c_str());
+   std::cout << "python exit code: " << code << std::endl;
+
+   h5features::reader reader("test.h5", "features");
+   std::cout << "version " << reader.version() << std::endl;
+   std::cout << "items: ";
+   for(const auto& item : reader.items())
+      std::cout << item << " ";
+   std::cout << std::endl;
+
+   for(const auto item : reader.read_all())
+   {
+      std::cout << item.name() << ": " << item.dim() << ", " << item.size()
+                << ", t: " << item.times().data()
+                << ", f: " << item.features().data()
+                << std::endl;
+      item.validate();
+   }
+}
+
+
+void write_cpp()
+{
+   const h5features::item item1{
+      "item1",
+      {{0, 1, 0, 1, 2, 0, 2, 3, 0}, 3},
+      {{0, 1, 1, 2, 2, 3}, h5features::times::format::interval}};
+
+   const h5features::item item2{
+      "item2",
+      {{10.1, 10, 10, 5.2, 5, 5, 1.3, 2, 3}, 3},
+      {{0, 1, 0.5, 1.5, 1, 2}, h5features::times::format::interval}};
+
+   const h5features::item item3{
+      "item3",
+      {{0, 1, 0, 1, 2, 0}, 3},
+      {{0, 1, 1, 2}, h5features::times::format::interval}};
+
+   std::cout << item1.name() << " " << item1.size() << " " << item1.dim() << std::endl;
+   std::cout << item2.name() << " " << item2.size() << " " << item2.dim() << std::endl;
+   std::cout << item3.name() << " " << item3.size() << " " << item3.dim() << std::endl;
+
+   h5features::writer writer("test.h5", "features", true, true, h5features::version::v1_1);
+   writer.write(item1);
+   writer.write(item2);
+   writer.write(item3);
+}
+
 int main()
 {
+//   read_v11();
+   //write_cpp();
+
    const auto item = generate_item("toto", 5, 2, false);
 
    {
       h5features::writer writer("./test.h5", "features", true, true, h5features::version::v1_1);
       writer.write(item);
+      std::cout << "wrote test.h5" << std::endl;
    }
 
    {
-      auto item2 = h5features::reader("./test.h5", "features").read_item("toto");
+      auto reader = h5features::reader("./test.h5", "features");
+      std::cout << "get reader: " << reader.items() << std::endl;
+
+      auto item2 = reader.read_item("toto");
       std::cout << (item2 == item) << std::endl;
    }
 
