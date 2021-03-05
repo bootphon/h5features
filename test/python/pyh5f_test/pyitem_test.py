5ccb646798947c2f86a244b85fb62ffeb49dfd4f
Gwendal Virlet <gwendal.virlet@inria.fr>
2021-03-05 09:24:00 +0100

    bind item properties

---
+++ test/python/pyh5f_test/pyitem_test.py
@@ -0,0 +1,57 @@
+import sys
+from h5features import Item
+from unittest import TestCase
+import psutil
+import numpy as np
+
+class ItemTests(TestCase):
+    """Tests on class Features"""
+    def test_properties(self):
+        features = np.ones((100,4))
+        begin = np.asarray([0, 1, 2, 3])
+        end = np.asarray([1, 2, 3, 4])
+        name = "Test"
+        properties = {
+            "int" : 1,
+            "double" : 1.,
+            "bool" : True,
+            "string" : "str",
+            "list of string" : ["str1", "str2"],
+            "list of int" : [1, 2, 3],
+            "list of double" : [1., 2., 3.],
+            "dict": {
+                "int " : 1,
+                "double" : 1.,
+                "bool" : True,
+                "string" : "str",
+                "list of string" : ["str1", "str2"],
+                "list of int" : [1, 2, 3],
+                "list of double" : [1., 2., 3.],
+        }}
+        item = Item(name, features, begin, end, properties, True)
+        props = item.properties()
+        assert props.get("int", None) is not None
+        assert props.get("double", None) is not None
+        assert props.get("bool", None) is not None
+        assert props.get("string", None) is not None
+        assert props.get("list of int", None) is not None
+        assert props.get("list of double", None) is not None
+        assert props.get("list of string", None) is not None
+        assert props.get("dict", None) is not None
+        assert props["int"] == 1
+        assert props["bool"]
+        assert props["double"] == 1.
+        assert props["string"] == "str"
+        assert props["list of string"] == ["str1", "str2"]
+        assert props["list of int"] == [1, 2, 3]
+        assert props["list of double"] == [1., 2., 3.]
+        assert props["dict"] == {
+                            "int " : 1,
+                            "double" : 1.,
+                            "bool" : True,
+                            "string" : "str",
+                            "list of string" : ["str1", "str2"],
+                            "list of int" : [1, 2, 3],
+                            "list of double" : [1., 2., 3.],
+        }
+
