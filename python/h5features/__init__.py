cb561c413b4fb1b50a0382c91472121d7dfa56a7
Mathieu Bernard <mathieu_bernard@laposte.net>
2021-06-21 17:24:56 +0200

    fixed version retrieving in Python

--- python/h5features/__init__.py
+++ python/h5features/__init__.py
@@ -5,14 +5,28 @@ from h5features.reader import Reader
 from h5features.writer import Writer
 
 
-# a hack to read the library version from setup.py
-import sys
-if sys.version_info >= (3, 8):
-    from importlib import metadata
-else:
-    import importlib_metadata as metadata
+try:
+    # a hack to read the library version from setup.py when h5features has been
+    # installed
+    import sys
+    if sys.version_info >= (3, 8):
+        from importlib import metadata
+    else:
+        import importlib_metadata as metadata
 
-__version__ = metadata.version('h5features')
+    __version__ = metadata.version('h5features')
+
+except metadata.PackageNotFoundError:
+    # h5features has not been installed, retrieve the version from the VERSION
+    # file directly
+    import pathlib
+    version_file = pathlib.Path(__file__).parent.parent.parent / 'VERSION'
+    try:
+        __version__ = open(version_file).read().strip()
+    except FileNotFoundError:
+        import warnings
+        warnings.warn('unable to retrieve h5features version')
+        __version__ = 'VERSION_NOT_FOUND'
 
 
 # exported when doing 'from h5features import *'
